31222f88d91b2b97dabb8fd00fc5baa5c69b5f3e
Willy Prado <pradopublicity@gmail.com>
2019-10-01 00:51:47 -0500

    Cambios en el CSS, visualización de los personajes en el index

--- script.js
+++ script.js
@@ -1,44 +1,177 @@
-// RickAndMorty
-// let masterURL = 'https://rickandmortyapi.com/api/'
-/*let page = "https://rickandmortyapi.com/api/"
-if (page) {
-  "characters": "https://rickandmortyapi.com/api/character",
-  "locations": "https://rickandmortyapi.com/api/location",
-  "episodes": "https://rickandmortyapi.com/api/episode"
-} */
-
-fetch('https://rickandmortyapi.com/api/character')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(myJson);
-    });
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+window.onload = () => {
+    dataRandom(0,494);
 }
 
-console.log(getRandomInt(494));
-// expected output: 0, 1 or 2
+//Contador aleatorio de personaje
+const dataRandom = (min, max) => {
+    let random1;
+    let random2;
+    let random3;
 
-console.log(getRandomInt(3));
-// expected output: 0
+    random1 = Math.floor(Math.random() * Math.floor(max, min)) + min;
+    random2 = Math.floor(Math.random() * Math.floor(max, min)) + min;
+    random3 = Math.floor(Math.random() * Math.floor(max, min)) + min;
 
-console.log(Math.random());
-// expected output: a number between 0 and 1
-
-
-
-
-
-
+    cardRandom(random1, random2, random3);
+}
 
 
-let img1 = document.getElementById('img1')
-let img2 = document.getElementById('img2')
-let img3 = document.getElementById('img3')
 
-// img1.src = 'https://rickandmortyapi.com/api/character/'
-// img2.src = 'https://rickandmortyapi.com/api/character/'
-// img3.src = 'https://rickandmortyapi.com/api/character/'
+let rick = []; //
+
+let image = []; //
+let name = []; //
+let id = [];
+let created = [];
+let status = []; //
+let gender = []; //
+let species = []; //
+let origin = []; //
+
+
+// llamado de la API
+const cardRandom = (random1, random2, random3) => {
+    fetch(`https://rickandmortyapi.com/api/character/${random1}, ${random2}, ${random3}`)
+    .then(data => data.json())
+    .then(data => {
+        let character1 = data[0]; //capturando el json del primer personaje aleatorio
+        let character2 = data[1]; //capturando el json del segundo personaje aleatorio
+        let character3 = data[2]; //capturando el json del tercer personaje aleatorio
+
+        //después de que se almacena la info de cada uno de los tres personajes se procede a entregarla.
+        let image1 = data[0].image;
+        let image2 = data[1].image;
+        let image3 = data[2].image;
+
+        let name1 = data[0].name;
+        let name2 = data[1].name;
+        let name3 = data[2].name;
+
+        let status1 = data[0].status;
+        let status2 = data[1].status;
+        let status3 = data[2].status;
+
+        let species1 = data[0].species;
+        let species2 = data[1].species;
+        let species3 = data[2].species;
+
+        let gender1 = data[0].gender;
+        let gender2 = data[1].gender;
+        let gender3 = data[2].gender;
+
+        let origin1 = data[0].origin[name];
+        let origin2 = data[1].origin[name];
+        let origin3 = data[2].origin[name];
+
+        
+
+        //entregando las imagenes al método que las cambiará en el HTML
+        changeImage1(image1);
+        changeImage2(image2);
+        changeImage3(image3);
+
+        changeName1(name1);
+        changeName2(name2);
+        changeName3(name3);
+
+        changeStatus1(status1);
+        changeStatus2(status2);
+        changeStatus3(status3);
+
+        changeSpecies1(species1);
+        changeSpecies2(species2);
+        changeSpecies3(species3);
+
+        changeGender1(gender1);
+        changeGender2(gender2);
+        changeGender3(gender3);
+
+        changeOrigin1(origin1);
+        changeOrigin2(origin2);
+        changeOrigin3(origin3);
+
+       
+
+        console.log(character1);
+        console.log(character2);
+        console.log(character3);
+        return data;
+        
+    });
+    
+}
+
+//cambiando las imágenes
+const changeImage1 = (image1) => {
+    document.getElementById("img1").src = image1;
+}
+
+const changeImage2 = (image2) => {
+    document.getElementById("img2").src = image2;
+}
+
+const changeImage3 = (image3) => {
+    document.getElementById("img3").src = image3; 
+}
+
+//cambiando los nombres
+const changeName1 = (name1) => {
+    document.getElementById("name1").innerText = name1;
+}
+
+const changeName2 = (name2) => {
+    document.getElementById("name2").innerText = name2;
+}
+
+const changeName3 = (name3) => {
+    document.getElementById("name3").innerText = name3;
+}
+
+//cambiando los status
+const changeStatus1 = (status1) => {
+    document.getElementById("status1").innerText = status1;
+}
+
+const changeStatus2 = (status2) => {
+    document.getElementById("status2").innerText = status2;
+}
+
+const changeStatus3 = (status3) => {
+    document.getElementById("status3").innerText = status3;
+}
+
+const changeSpecies1 = (species1) => {
+    document.getElementById("spe1").innerText = species1;
+}
+
+const changeSpecies2 = (species2) => {
+    document.getElementById("spe2").innerText = species2;
+}
+
+const changeSpecies3 = (species3) => {
+    document.getElementById("spe3").innerText = species3;
+}
+
+const changeGender1 = (gender1) => {
+    document.getElementById("gen1").innerText = gender1;
+}
+
+const changeGender2 = (gender2) => {
+    document.getElementById("gen2").innerText = gender2;
+}
+
+const changeGender3 = (gender3) => {
+    document.getElementById("gen3").innerText = gender3;
+}
+
+const changeOrigin1 = (origin1) => {
+    document.getElementById("ori1").innerText = origin1;
+}
+
+const changeOrigin2 = (origin2) => {
+    document.getElementById("ori2").innerText = origin2;
+}
+
+const changeOrigin3 = (origin3) => {
+    document.getElementById("ori3").innerText = origin3;
+}
